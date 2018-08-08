2d8eb88143405e97ed321d1e4c4de8aa3390992d
ruoshuiqinmo0 <609392790@qq.com>
2018-08-08 18:09:22 +0800

    2018-08-08

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,6 +7,7 @@ class Index extends Controller
 {
     public function index()
     {
+       echo json_encode(['errcode'=>0,'msg'=>'ok','token'=>'11111','code'=>123456,'login'=>1,'fee'=>'a123456','phone'=>'13617478003']);
     	return view();
     }
 }
