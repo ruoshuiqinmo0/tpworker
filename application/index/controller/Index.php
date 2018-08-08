91ac6dc0f3e39b25eee747a38748e64643d5bdef
ruoshuiqinmo0 <609392790@qq.com>
2018-08-09 00:32:41 +0800

    update Work for uid

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,6 +8,14 @@ class Index extends Controller
     public function index()
     {
        echo json_encode(['errcode'=>0,'msg'=>'ok','token'=>'11111','code'=>123456,'login'=>1,'fee'=>'a123456','phone'=>'13617478003']);
+        echo json_encode([
+            'errorcode'=>123,
+            'messag'=>[
+                ['ok'=>1,2,3],
+                'message'=>[4,5,6],
+                'code'=>[7,8,9]
+            ]
+        ]);
     	return view();
     }
 }
